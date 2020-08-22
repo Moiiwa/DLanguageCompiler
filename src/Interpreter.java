c6802cf553c18b6dc07465934e2864770b6a07ce
mihailgudkov <moiiwa@icloud.com>
2020-08-22 18:53:06 +0300

    Initial commit

---
+++ src/Interpreter.java
@@ -0,0 +1,12 @@
+import java.util.Iterator;
+
+public class Interpreter {
+
+    public static void main(String[] args) {
+        Iterator token = Tokens.allTokens.iterator();
+        while (token.hasNext()){
+            System.out.println(token.next());
+        }
+        System.out.println(Tokens.allTokens.size());
+    }
+}
